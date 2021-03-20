9a3ade6adff34313ca96eb403f59b6b850824e81
rishabhsharma015 <60418660+rishabhsharma015@users.noreply.github.com>
2021-03-21 00:11:12 +0530

    Add files via upload
    Done

---
+++ Github notifier.py
@@ -0,0 +1,12 @@
+import time
+from plyer import notification
+
+if __name__ == "__main__":
+
+    notification.notify(
+            title = "GITHUB commit notification",
+            message = "It's time to commit on GITHUB!!",
+            app_icon = "Danleech-Simple-Github.ico",
+            timeout = 5
+
+        )
